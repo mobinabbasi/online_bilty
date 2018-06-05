c6244688e839af04efc929c5caafe8dceb4dc774
Prasoon Tare <35687260+prasoon-tare@users.noreply.github.com>
2018-06-05 19:07:02 +0530

    Revert "Changes and new screens update in UI Design"

--- src/pages/search-location/search-location.ts
+++ src/pages/search-location/search-location.ts
@@ -1,50 +1,74 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
+import { HttpClient } from '@angular/common/http';
+import { error } from '@firebase/database/dist/esm/src/core/util/util';
+import {ServiceProvider} from '../../providers/service/service';
 
-/**
- * Generated class for the SearchLocationPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
 
 @IonicPage()
 @Component({
   selector: 'page-search-location',
   templateUrl: 'search-location.html',
+  providers: [ServiceProvider],
 })
 export class SearchLocationPage {
   searchQuery: string = '';
+  cities: string[];
+  ErrorMessage: string;
+ // city:any;
   items: string[];
 
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    this.initializeItems();
+  constructor(public navCtrl: NavController, public navParams: NavParams,private service: ServiceProvider,
+    public http: Http, public httpclient: HttpClient) {
+    //this.initializeItems();
+    this.GetCity();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad SearchLocationPage');
-  }
-  initializeItems() {
-    this.items = [
-      'Amsterdam',
-      'Bogota',
-    ];
+  
+  // initializeItems() {
+  //   this.items = [
+  //     'Amsterdam',
+  //     'Bogota',
+  //   ];
+  // }
+
+
+   GetCity() {
+    //let city = new Array();
+    this.service.getCity().subscribe(
+      // cities => this.cities = cities,
+      // error => this.ErrorMessage = <any>error
+      (data) => {
+
+        for(let key of data) {
+          this.cities.push(data[key]);
+        }
+        console.log(this.cities);
+      },(error) => console.log('Error', error));
+    
+    // let val = [];
+    // for(var i of this.cities.data) {
+    //   val.push(i.city_name);
+    //   console.log(val);
+    // }
+
   }
 
-  getItems(ev: any) {
-    // Reset items back to all of the items
-    this.initializeItems();
+  // getItems(ev: any) {
+  //   // Reset items back to all of the items
+  //  // this.GetCity();
 
-    // set val to the value of the searchbar
-    let val = ev.target.value;
+  //   // set val to the value of the searchbar
+  //   let val = ev.target.value;
 
-    // if the value is an empty string don't filter the items
-    if (val && val.trim() != '') {
-      this.items = this.items.filter((item) => {
-        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
-      })
-    }
-  }
+  //   // if the value is an empty string don't filter the items
+  //   if (val && val.trim() != '') {
+  //     this.items = this.items.filter((item) => {
+  //       return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+  //     })
+  //   }
+  // }
 
 }
